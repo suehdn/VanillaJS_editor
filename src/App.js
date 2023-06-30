ab1b1db50d8fa8f1c8c463757c0b35a801fbe1d2
AhnHyesu <linim@naver.com>
2023-06-30 17:28:33 +0900

    feat: 더미데이터 -> 서버 데이터로 교체

--- src/App.js
+++ src/App.js
@@ -2,45 +2,23 @@ import Data from './data.js';
 import SideBar from './SideBar.js';
 import Editor from './Editor.js';
 
-const DUMMY_DATA = [
-    {
-        "id": 1, // Document id
-        "title": "노션을 만들자", // Document title
-        "documents": [
-            {
-                "id": 2,
-                "title": "블라블라",
-                "documents": [
-                    {
-                        "id": 3,
-                        "title": "함냐함냐",
-                        "documents": []
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "id": 4,
-        "title": "hello!",
-        "documents": []
-    }
-]
-
 export default class App {
     constructor({ $target }) {
         this.$target = $target;
         const data = new Data();
-        const sideBar = new SideBar({
-            $target: this.$target,
-            initialState: DUMMY_DATA
+        data.getDocumentStructure().then(x => {
+            const sideBar = new SideBar({
+                $target: this.$target,
+                initialState: x
+            })
+            const editor = new Editor({
+                $target: this.$target,
+                initialState: x
+            })
         })
-        const editor = new Editor({
-            $target: this.$target,
-            initialState: DUMMY_DATA
-        })
-
     }
+
+
 }
 
 
