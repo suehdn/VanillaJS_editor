9e1eae52bf572b0cbde06f7e8b078e6de8f230fb
AhnHyesu <linim@naver.com>
2023-07-01 17:03:45 +0900

    feat: 편집기에 입력한 문자열을 this.state에 넣어주는 작업

--- src/Editor.js
+++ src/Editor.js
@@ -1,11 +1,27 @@
 export default class Editor {
-    constructor({ $target, initialstate }) {
+    constructor({ $target, initialstate = { title: '', content: '' } }) {
         this.$editor = document.createElement('div');
         this.$target = $target;
         this.state = initialstate;
         this.makeEditor();
+        this.eventAdd();
     }
 
+    setState = (nextState) => {
+        this.state = nextState;
+        this.render();
+    }
+
+    render() {
+        console.log(this.$editor.querySelector('[name=title]').value, this.$editor.querySelector('[name=content]').innerHTML)
+        // this.$editor.querySelector('[name=title]').value = this.state.title;
+        // this.$editor.querySelector('[name=content]').innerHTML = this.state.content;
+    }
+
+
+    /**
+     *  문서 편집기를 만드는 함수
+     */
     makeEditor() {
         this.$editor.className = 'editor__div--flex'
         this.$target.appendChild(this.$editor);
@@ -14,5 +30,26 @@ export default class Editor {
             <input name="title" type="text" placeholder = "제목 없음" class = "editor__input"/>
             <div name="content" contentEditable="true" placeholedr = "내용을 입력하세요." class = "editor__content"></div>
         `
+    }
+    /**
+     * 문자를 입력했을 때 입력 받은 문자열을 this.state에 넣어주는 함수
+     */
+    eventAdd() {
+        this.$editor.querySelector('[name=title]').addEventListener('keyup', (e) => {
+            const nextState = {
+                ...this.state,
+                title: e.target.value
+            }
+            console.log(nextState);
+            this.setState(nextState);
+        })
+        this.$editor.querySelector('[name=content]').addEventListener('input', (e) => {
+            const nextState = {
+                ...this.state,
+                content: e.target.innerHTML
+            }
+            console.log(nextState);
+            this.setState(nextState);
+        })
     }
 }
