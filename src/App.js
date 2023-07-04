f10559728edd8fe964ff7384d215cff075f27a6f
AhnHyesu <linim@naver.com>
2023-07-04 15:39:55 +0900

    feat: 라우팅 처리 완료

--- src/App.js
+++ src/App.js
@@ -9,43 +9,43 @@ export default class App {
         const data = new Data();
 
         data.getDocumentStructure().then(x => {
+            this.editor = new Editor({
+                $target: this.$target,
+                initialState: {},
+                onEditing: (post) => {
+                    if (this.timer !== null) {
+                        clearTimeout(this.timer);
+                    }
+                    if (this.sideBar.state.postId) {
+                        this.timer = setTimeout(async () => {
+                            this.data.editDocument(this.sideBar.state.postId, post.title, post.content);
+                            this.render();
+                        }, 1000)
+                    }
+                }
+            })
             this.sideBar = new SideBar({
                 $target: this.$target,
                 initialState: { list: x },
                 editorsetState: this.editor.setState
             })
-        })
-        this.editor = new Editor({
-            $target: this.$target,
-            initialState: {},
-            onEditing: (post) => {
-                if (this.timer !== null) {
-                    clearTimeout(this.timer);
-                }
-                if (this.sideBar.state.postId) {
-                    this.timer = setTimeout(async () => {
-                        this.data.editDocument(this.sideBar.state.postId, post.title, post.content);
-                        this.render();
-                    }, 1000)
-                }
-            }
+            this.route();
         })
 
 
-        this.route();
+
         initRouter(() => this.route());
     }
 
     route = () => {
-        //     const {pathname} = window.location;
+        const { pathname } = window.location;
 
-        //     if(pathname === '/'){
-
-        //     }
+        if (pathname.indexOf('/posts/') === 0) {
+            const [, , postId] = pathname.split('/');
+            console.log(this.sideBar);
+            this.sideBar.setState({ postId: postId })
+        }
     }
-
-
-
 }
 
 
