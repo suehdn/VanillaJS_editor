b4f5f4d3efb885e58aa6530f0a258fa13e68a7a1
AhnHyesu <67812466+suehdn@users.noreply.github.com>
2023-07-06 15:18:07 +0900

    fix: 주소 변경

--- src/api.js
+++ src/api.js
@@ -17,6 +17,6 @@ export const request = async (url = "", options = {}) => {
         throw new Error('API 처리중 이상이 발생했습니다.');
     } catch (event) {
         alert(event.message);
-        window.location = "http://localhost:3000"
+        window.location = "https://fedc-4-5-project-notion-vanilla-js-rosy.vercel.app/"
     }
 }
