1c3e92c4513e5d54bf8f4c4dd053eea2c1627541
Ahnhyesu <linim@naver.com>
2024-08-15 21:54:15 +0900

    feat : #15 - SideBarHeader 컴포넌트 분리

---
+++ src/components/SideBarHeader.js
@@ -0,0 +1,11 @@
+export default class SideBarHeader {
+  constructor({ $target }) {
+    this.$sideBarHeader = document.createElement("section");
+    this.$sideBarHeader.className = "sidebar__section--name";
+    this.$sideBarHeader.innerHTML = `H의 Notion
+                <div class = 'filePage__root-add--add'>         
+                <button class = "filePage__button--add" data-id="null"><span class = 'filePage__root-add--add'>새로운 페이지 추가 <img src="../png/square_plus_icon_24.png"></span></button>
+            </div>`;
+    $target.appendChild(this.$sideBarHeader);
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as SideBarHeader } from "./SideBarHeader";
