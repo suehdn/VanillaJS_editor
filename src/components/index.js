db76f6ced8bbf8870bd7ad1197bff20e3d0fd773
Ahnhyesu <linim@naver.com>
2024-08-16 14:01:45 +0900

    feat : #15 - 만들어 놓은 사이드바 컴포넌트 export 처리 완료

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as SideBarHeader } from "./SideBar/SideBarHeader";
+export { default as SideBarPages } from "./SideBar/SideBarPages";
+export { default as SideBarPagesDetails } from "./SideBar/SideBarPagesDetails";
