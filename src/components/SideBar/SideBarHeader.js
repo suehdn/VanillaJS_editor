e2a30eedd6eebf5ea5a3ead6549a56a21b86d888
Ahnhyesu <linim@naver.com>
2024-08-16 13:59:48 +0900

    feat : #15 - SidebarHeader 클래스 이름 수정, span 수정

--- src/components/SideBar/SideBarHeader.js
+++ src/components/SideBar/SideBarHeader.js
@@ -1,10 +1,12 @@
 export default class SideBarHeader {
   constructor({ $target }) {
     this.$sideBarHeader = document.createElement("section");
-    this.$sideBarHeader.className = "sidebar__section--header";
-    this.$sideBarHeader.innerHTML = `H의 Notion
+    this.$sideBarHeader.className = "sidebar__header";
+    this.$sideBarHeader.innerHTML = `
+                <span>H의 Notion</span>
                 <div class = 'filePage__root-add--add'>         
-                <button class = "filePage__button--add" data-id="null"><span class = 'filePage__root-add--add'>새로운 페이지 추가 <img src="../png/square_plus_icon_24.png"></span></button>
+                <button class = "filePage__button--add" data-id="null">
+                <span class = 'filePage__root-add--add'>새로운 페이지 추가 <img src="../png/square_plus_icon_24.png"></span></button>
             </div>`;
     $target.appendChild(this.$sideBarHeader);
   }
