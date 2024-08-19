a10aa9668d9e37c73ee8e9a76008e3496f30721e
Ahnhyesu <linim@naver.com>
2024-08-19 17:43:06 +0900

    feat : #15 - 구글 아이콘 추가 후 detail hover시 아이콘 변경 및 배경색 변경 추가

--- src/components/SideBar/SideBarPagesDetails.js
+++ src/components/SideBar/SideBarPagesDetails.js
@@ -7,6 +7,7 @@ export default class SideBarPagesDetails {
     this.$sideBarPagesDetails.className = "sidebar__pages--detail";
 
     this.render();
+    this.eventAdd();
   }
 
   render() {
@@ -14,8 +15,8 @@ export default class SideBarPagesDetails {
       <div class = 'sidebar__pages--detail-container' style = '--depth: ${
         this.$depth
       }'>
-        <button data-action='toggle'>
-          <i class = "sidebar"> > </i>
+        <button class = "sidebar__pages--detail-button" data-action='toggle'>
+          <span class = "material-symbols-rounded"> description </span>
         </button>
         <div class = 'sidebar__pages--detail-title'>
           ${this.state.title || "제목 없음"}
@@ -23,5 +24,25 @@ export default class SideBarPagesDetails {
       </div>
     `;
     this.$target.appendChild(this.$sideBarPagesDetails);
+  }
+
+  eventAdd() {
+    const button = this.$sideBarPagesDetails.querySelector(
+      ".sidebar__pages--detail-button"
+    );
+    const icon = button.querySelector(".material-symbols-rounded");
+    let isHovered = false;
+
+    this.$sideBarPagesDetails.addEventListener("mouseover", () => {
+      if (!isHovered) {
+        icon.textContent = "keyboard_arrow_right";
+        isHovered = true;
+      }
+    });
+
+    this.$sideBarPagesDetails.addEventListener("mouseleave", () => {
+      icon.textContent = "description";
+      isHovered = false;
+    });
   }
 }
