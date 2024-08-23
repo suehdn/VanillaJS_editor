2e4005886cff85de3d1d3c3317a5c15bc1630dc7
Ahnhyesu <linim@naver.com>
2024-08-24 01:21:12 +0900

    feat : #15 - 사이드바 헤더 1차 완성 (add a page 후 렌더링은 되지 않음)

--- src/components/SideBar/SideBarHeader.js
+++ src/components/SideBar/SideBarHeader.js
@@ -1,13 +1,80 @@
+import { push } from "@/router";
+import Data from "@/data";
+import { setItem, getItem } from "@/store/localStorage";
+
 export default class SideBarHeader {
   constructor({ $target }) {
+    this.$target = $target;
     this.$sideBarHeader = document.createElement("section");
     this.$sideBarHeader.className = "sidebar__header";
-    this.$sideBarHeader.innerHTML = `
-                <span>H의 Notion</span>
-                <div class = 'filePage__root-add--add'>         
-                <button class = "filePage__button--add" data-id="null">
-                <span class = 'filePage__root-add--add'>새로운 페이지 추가 <img src="../png/square_plus_icon_24.png"></span></button>
-            </div>`;
-    $target.appendChild(this.$sideBarHeader);
+    this.data = new Data();
+    this.$beforeSelected = 0;
+
+    this.initialize();
+    this.eventAdd();
   }
+
+  initialize = () => {
+    this.$sideBarHeader.innerHTML = `
+                <span class = 'sidebar__header--main sidebar__header--action' data-action = 'main'>H의 Notion</span>
+                <div class = 'sidebar__header--container'>         
+                  <div class = 'sidebar__header--action' data-action = 'quick_start'>  
+                    <span class = "material-symbols-rounded">bolt</span>
+                    <span>Quick start</span>
+                  </div>
+                  <div class = 'sidebar__header--action' data-action = 'guestbook'>  
+                    <span class = "material-symbols-rounded">menu_book</span>
+                    <span>Guestbook</span>
+                  </div>
+                  <div class = 'sidebar__header--action' data-action = 'add'>  
+                    <span class = "material-symbols-rounded">edit_square</span>
+                    <span>Add a page</span>
+                  </div>
+                </div>`;
+    this.$target.appendChild(this.$sideBarHeader);
+  };
+
+  render = () => {
+    const selected = getItem("selected");
+
+    document
+      .querySelector(
+        `.sidebar__pages--detail[data-id="${this.$beforeSelected}"]`
+      )
+      ?.classList.remove("highlight");
+
+    document
+      .querySelector(`.sidebar__pages--detail[data-id="${selected}"]`)
+      ?.classList.add("highlight");
+  };
+
+  eventAdd = () => {
+    this.$sideBarHeader.addEventListener("click", this.clickEventAdd);
+  };
+
+  clickEventAdd = async (e) => {
+    if (e.target.closest(".sidebar__header--action")) {
+      const action = e.target.closest(".sidebar__header--action").dataset
+        .action;
+      switch (action) {
+        case "main":
+          push(`/`);
+          break;
+        case "add":
+          await this.data.addDocumentStructure().then((x) => {
+            push(`/${x.id}`);
+            this.$beforeSelected = getItem("selected");
+            setItem("selected", x.id);
+            this.render();
+          });
+          break;
+        case "quick_start":
+          push(`/quick_start`);
+          break;
+        case "guestbook":
+          push(`/guestbook`);
+          break;
+      }
+    }
+  };
 }
