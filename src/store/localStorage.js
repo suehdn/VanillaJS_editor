cec8dfed7941f92354d052a2dffeb0950cf68ba9
Ahnhyesu <linim@naver.com>
2024-08-23 16:48:11 +0900

    feat : #15 - localStorage object일 때 spread 연산자 추가

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -1,27 +1,28 @@
 const storage = window.localStorage;
 
 export const setItem = (key, value) => {
-    try {
-        storage.setItem(key, JSON.stringify(value));
-    }
-    catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    if (typeof value === "object") {
+      storage.setItem(key, JSON.stringify([...value]));
+    } else storage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const getItem = (key, defaultValue) => {
-    try {
-        const storedValue = storage.getItem(key);
-        if (!storedValue) {
-            return defaultValue;
-        }
-        const parsedValue = JSON.parse(storedValue);
-        return parsedValue;
-    } catch {
-        return defaultValue;
+  try {
+    const storedValue = storage.getItem(key);
+    if (!storedValue) {
+      return defaultValue;
     }
-}
+    const parsedValue = JSON.parse(storedValue);
+    return parsedValue;
+  } catch {
+    return defaultValue;
+  }
+};
 
 export const removeItem = (key) => {
-    storage.removeItem(key);
-}
+  storage.removeItem(key);
+};
