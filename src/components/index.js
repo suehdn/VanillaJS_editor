6b703a95528a528a9e0c65dfbc78dfb94a2c3d91
Ahnhyesu <linim@naver.com>
2024-08-29 20:17:29 +0900

    feat : #16 - SideBarPagesDetails 제거

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as SideBarHeader } from "./SideBar/SideBarHeader";
 export { default as SideBarPages } from "./SideBar/SideBarPages";
-export { default as SideBarPagesDetails } from "./SideBar/SideBarPagesDetails";
