181434d457edaa1ac4c7aef26a1b68edab20c0b5
Ahnhyesu <linim@naver.com>
2024-08-29 20:07:18 +0900

    feat : #16 - 컴포넌트 virtualDOM 적용

--- src/core/Component.js
+++ src/core/Component.js
@@ -6,8 +6,8 @@ export default class Component {
     this.$target = $target;
     this.props = props;
     this.setup();
-    this.setEvent();
     this.render();
+    this.setEvent();
   }
 
   setup() {}
@@ -16,7 +16,17 @@ export default class Component {
     return "";
   }
   render() {
-    this.$target.innerHTML = this.template();
+    const { $target } = this;
+    const virtualNode = $target.cloneNode(true);
+
+    virtualNode.innerHTML = this.template();
+
+    const realChildNodes = [...$target.childNodes];
+    const virtualChildNodes = [...virtualNode.childNodes];
+    const max = Math.max(realChildNodes.length, virtualChildNodes.length);
+    for (let i = 0; i < max; i++) {
+      updateNode($target, realChildNodes[i], virtualChildNodes[i]);
+    }
     this.mounted();
   }
   setEvent() {}
@@ -32,3 +42,39 @@ export default class Component {
     });
   }
 }
+
+const updateNode = (parent, realNode, virtualNode) => {
+  if (realNode && !virtualNode) {
+    return realNode.remove();
+  }
+  if (!realNode && virtualNode) {
+    return parent.appendChild(virtualNode);
+  }
+  if (realNode instanceof Text && virtualNode instanceof Text) {
+    if (realNode.nodeValue === virtualNode.nodeValue) return;
+    return (realNode.nodeValue = virtualNode.nodeValue);
+  }
+  if (realNode.nodeName !== virtualNode.nodeName) {
+    const index = [...parent.childNodes].indexOf(realNode);
+    return realNode.remove(), parent.appendChild(virtualNode, index);
+  }
+  updateAttributes(realNode, virtualNode);
+
+  const virtualChildren = [...virtualNode.childNodes];
+  const realChildren = [...realNode.childNodes];
+  const max = Math.max(realChildren.length, virtualChildren.length);
+  for (let i = 0; i < max; i++) {
+    updateNode(realNode, realChildren[i], virtualChildren[i]);
+  }
+};
+
+const updateAttributes = (realNode, virtualNode) => {
+  for (const { name, value } of [...virtualNode.attributes]) {
+    if (value === realNode.getAttribute(name)) continue;
+    realNode.setAttribute(name, value);
+  }
+  for (const { name } of [...realNode.attributes]) {
+    if (virtualNode.getAttribute(name) !== undefined) continue;
+    realNode.removeAttribute(name);
+  }
+};
