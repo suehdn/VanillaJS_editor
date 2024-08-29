dfe9c79b416aaa11f23fdff8076b0e27b32fdf5d
Ahnhyesu <linim@naver.com>
2024-08-29 23:32:58 +0900

    feat : #15 - add, delete 기능 완료 & mouseover시 아이콘 변경 기능 완료

--- src/components/SideBar/SideBarPages.js
+++ src/components/SideBar/SideBarPages.js
@@ -8,11 +8,18 @@ export default class SideBarPages extends Component {
     this.state = {
       pages: this.props,
       openedDetail: new Set(getItem("openedDetail", [])),
+      hoveredId: null,
     };
     this.data = new Data();
+    this.hoveredElementId = null;
   }
   template() {
-    return `${printPage(this.state.pages, this.state.openedDetail)}`;
+    return `${printPage(
+      this.state.pages,
+      this.state.openedDetail,
+      0,
+      this.hoveredElementId
+    )}`;
   }
   mounted() {}
   setEvent() {
@@ -27,47 +34,47 @@ export default class SideBarPages extends Component {
         eventArea.classList.remove("scrolled");
       }
     });
+
     this.addEvent("click", ".sidebar__pages--detail-click", async (e) => {
       const action = e.target.closest(".sidebar__pages--detail-click").dataset
         .action;
       const id = e.target.closest(".sidebar__pages--detail").dataset.id;
+      const setOpenedDetail = (action, id) => {
+        const tempOpenedDetail = new Set(this.state.openedDetail);
+        if (action === "add" || !tempOpenedDetail.has(id)) {
+          tempOpenedDetail.add(id);
+        } else if (action === "delete" || tempOpenedDetail.has(id)) {
+          tempOpenedDetail.delete(id);
+        }
+        return tempOpenedDetail;
+      };
+      const getPages = (action, id) => {
+        const tempOpenedDetail = setOpenedDetail(action, id);
+        if (action === "select") {
+          // this.setState({ openedDetail: tempOpenedDetail });
+        } else {
+          this.data.getDocumentStructure().then((pages) => {
+            this.setState({ pages, openedDetail: tempOpenedDetail });
+          });
+        }
+      };
       switch (action) {
         case "toggle":
-          const tempOpendeDetail = new Set(this.state.openedDetail);
-          if (!tempOpendeDetail.has(id)) {
-            tempOpendeDetail.add(id);
-          } else tempOpendeDetail.delete(id);
-          setItem("openedDetail", tempOpendeDetail);
-          console.log("Toggle action triggered");
-          // console.log(this.template());
-          // console.log("현재 타겟:", this.$target);
-          this.setState({ openedDetail: tempOpendeDetail });
-          // this.render();
+          const tempOpenedDetail = setOpenedDetail(action, id);
+          setItem("openedDetail", tempOpenedDetail);
+          this.setState({ openedDetail: tempOpenedDetail });
           break;
         case "remove":
           await this.data.deleteDocumentStructure(id).then(() => {
             push(`/`);
-            //   console.log("deleted: ", this.state.id);
-            //   console.log(this.$sideBarPagesDetails.parentNode);
-            //   this.setDetail(
-            //     this.findParentId(this.$sideBarPagesDetails, this.state.id),
-            //     "remove"
-            //   );
+            getPages("remove", id);
           });
-          // console.log(
-          //   this.findParentId(this.$sideBarPagesDetails, this.state.id)
-          // );
-          console.log("Remove action triggered");
           break;
         case "add":
           await this.data.addDocumentStructure(id).then((x) => {
             push(`/${x.id}`);
-            //   this.setDetail(this.state.id, "add");
-            //   this.$beforeSelected = getItem("selected");
-            //   setItem("selected", x.id);
-            //   this.render();
+            getPages("add", id);
           });
-          console.log("Add action triggered");
           break;
         case "select":
           console.log("Select action triggered");
@@ -78,6 +85,34 @@ export default class SideBarPages extends Component {
           break;
       }
     });
+
+    this.addEvent(
+      "mouseover",
+      ".sidebar__pages--detail-button[data-action=toggle]",
+      async (e) => {
+        const icon = e.target.closest(
+          ".sidebar__pages--detail-button .material-symbols-rounded"
+        );
+        this.hoveredElementId = e.target.closest(
+          ".sidebar__pages--detail"
+        ).dataset.id;
+        if (this.state.openedDetail.has(this.hoveredElementId)) {
+          icon.textContent = "keyboard_arrow_down";
+        } else icon.textContent = "keyboard_arrow_right";
+      }
+    );
+
+    this.addEvent(
+      "mouseout",
+      ".sidebar__pages--detail-button[data-action=toggle]",
+      async (e) => {
+        const icon = e.target
+          .closest(".sidebar__pages--detail-button")
+          .querySelector(".material-symbols-rounded");
+        icon.textContent = "description";
+        this.hoveredElementId = null;
+      }
+    );
   }
 }
 
@@ -88,17 +123,24 @@ export default class SideBarPages extends Component {
  * @param {*} depth 페이지의 깊이 (들여쓰기 깊이)
  */
 // prettier-ignore
-const printPage = (pageList, openedDetail, depth = 0) => {
+const printPage = (pageList, openedDetail, depth = 0, hoveredElementId) => {
   let content = '<ul class="sidebar__pages--container">';
+  let icon = null;
   pageList.forEach((list, index) => {
+    if(String(list.id) !== hoveredElementId){
+      icon =  "description";
+    }else{
+      if(openedDetail.has(String(list.id))) icon = "keyboard_arrow_down";
+      else icon =  "keyboard_arrow_right";
+    }
     content += `
       <li class="sidebar__pages--detail sidebar__pages--detail-click" data-id="${list.id}" data-action="select" style="--depth:${depth}">
         <div class="sidebar__pages--detail-contents">
-          <button class="sidebar__pages--detail-button sidebar__pages--detail-click" data-action="toggle">
-            <span class="material-symbols-rounded"> description </span>
-          </button>
+          <button class="sidebar__pages--detail-button sidebar__pages--detail-click" data-action="toggle">` +
+            `<span class="material-symbols-rounded"> ${icon} </span> `+
+          `</button>
           <span class="sidebar__pages--detail-title">
-            ${list.title || "제목 없음"}
+            ${list.id || "제목 없음"}
           </span>
         </div>
         <div class="sidebar__pages--detail-toolkit">
@@ -112,7 +154,7 @@ const printPage = (pageList, openedDetail, depth = 0) => {
       </li>`;
     if (openedDetail.has(String(list.id))) {
       if (list.documents.length) {
-        content += printPage(list.documents, openedDetail, depth + 1);
+        content += printPage(list.documents, openedDetail, depth + 1,hoveredElementId);
       } else {
         content += `<div class="sidebar__pages--empty" style="--depth:${depth + 1}">하위 페이지 없음</div>`;
       }
