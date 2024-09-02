bf97d4c9df7ac201a7b7706d00f9ec0a383fbc3a
Ahnhyesu <linim@naver.com>
2024-09-02 20:58:37 +0900

    feat : #21 - EditorTitle 컴포넌트 추가

---
+++ src/components/Editor/EditorTitle.js
@@ -0,0 +1,10 @@
+import { Component } from "@core";
+
+export default class EditorTitle extends Component {
+  setup() {}
+  template() {
+    return `
+      <input name="title" type="text" placeholder = "Untitled" class = "editor__input-title" value = ""/>
+      `;
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SideBarHeader } from "./SideBar/SideBarHeader";
 export { default as SideBarPages } from "./SideBar/SideBarPages";
+export { default as EditorTitle } from "./Editor/EditorTitle";
