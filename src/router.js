ea61d7e78124d40a276cae58056db1f00434c1c0
Ahnhyesu <linim@naver.com>
2024-09-02 22:00:28 +0900

    feat : #21 - URL에서 documentID를 가져오는 함수 router에 추가

--- src/router.js
+++ src/router.js
@@ -1,20 +1,27 @@
-const ROUTE_CHANGE_EVENT_NAME = 'route-change';
+const ROUTE_CHANGE_EVENT_NAME = "route-change";
 
 export const initRouter = (onRoute) => {
-    window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
-        const { nextUrl } = e.detail;
+  window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
+    const { nextUrl } = e.detail;
 
-        if (nextUrl) {
-            history.pushState(null, null, nextUrl);
-            onRoute();
-        }
-    })
-}
+    if (nextUrl) {
+      history.pushState(null, null, nextUrl);
+      onRoute();
+    }
+  });
+};
 
 export const push = (nextUrl) => {
-    window.dispatchEvent(new CustomEvent(ROUTE_CHANGE_EVENT_NAME, {
-        detail: {
-            nextUrl
-        }
-    }))
-}
+  window.dispatchEvent(
+    new CustomEvent(ROUTE_CHANGE_EVENT_NAME, {
+      detail: {
+        nextUrl,
+      },
+    })
+  );
+};
+
+export const getDocumentId = () => {
+  const urlList = window.location.href.split("/");
+  return urlList[urlList.length - 1];
+};
