148a60dd7482bddfa8a4209f75c01375cb3cff5a
Ahnhyesu <linim@naver.com>
2024-09-04 01:43:56 +0900

    feat : #21 - executeWithTryCatch SideBar에 적용

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,24 +2,27 @@ import Data from "./data.js";
 import { Component } from "@core";
 import { SideBarHeader, SideBarPages } from "@components";
 import { getItem } from "./stores/localStorage.js";
+import { executeWithTryCatch } from "@utils";
 
 export default class SideBar extends Component {
   setup() {
     this.data = new Data();
     this.sideBarPages = null;
   }
-  mounted() {
+  async mounted() {
     const $sidebarHeader = this.$target.querySelector(".sidebar__header");
     const $sidebarPages = this.$target.querySelector(".sidebar__pages");
 
     new SideBarHeader({
       $target: $sidebarHeader,
     });
-    this.data.getDocumentStructure().then((x) => {
+
+    await executeWithTryCatch(async () => {
+      const pages = await this.data.getDocumentStructure();
       this.sideBarPages = new SideBarPages({
         $target: $sidebarPages,
-        props: x,
+        props: pages,
       });
-    });
+    }, "Error get document structure SideBar");
   }
 }
