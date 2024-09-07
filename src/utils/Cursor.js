d79fe07d3c4730822d6033466ec7bf09698dcb46
Ahnhyesu <linim@naver.com>
2024-09-07 16:58:40 +0900

    feat : #22 - 백스페이스 후 캐럿 위치가 제일 앞으로 지정되는 문제 해결을 위해 setCaretAtEnd 함수 추가

--- src/utils/Cursor.js
+++ src/utils/Cursor.js
@@ -1,7 +1,23 @@
 export const setCaretOffset = () => {
   const selection = window.getSelection();
+  console.log(selection.focusNode);
   const range = selection.getRangeAt(0);
   return range.startOffset;
+};
+
+export const setCaretAtEnd = (element) => {
+  const range = document.createRange();
+  const selection = window.getSelection();
+
+  // 텍스트가 있는 경우 마지막 텍스트 노드를 선택
+  const textNode = element.lastChild;
+
+  if (textNode) {
+    range.setStart(textNode, textNode.length);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 };
 
 export const saveCursor = (targetDiv, caretOffset) => {
